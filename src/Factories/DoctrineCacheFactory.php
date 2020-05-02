f67733ca281a7650edff68e3e19149afcc48e451
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-05-02 21:41:15 +0100

    Can now generate Doctrine filesystem cache instance

--- src/Factories/DoctrineCacheFactory.php
+++ src/Factories/DoctrineCacheFactory.php
@@ -6,8 +6,10 @@ namespace PublishingKit\Cache\Factories;
 
 use Cache\Adapter\Doctrine\DoctrineCachePool;
 use Doctrine\Common\Cache\Cache;
+use Doctrine\Common\Cache\FilesystemCache;
 use Psr\Cache\CacheItemPoolInterface;
 use PublishingKit\Cache\Contracts\Factories\CacheFactory;
+use PublishingKit\Cache\Exceptions\Factories\PathNotSet;
 
 final class DoctrineCacheFactory implements CacheFactory
 {
@@ -18,5 +20,22 @@ final class DoctrineCacheFactory implements CacheFactory
 
     private function createAdapter(array $config): Cache
     {
+        if (!isset($config['driver'])) {
+            $config['driver'] = 'filesystem';
+        }
+        switch ($config['driver']) {
+            default:
+                $driver = $this->createFilesystemAdapter($config);
+                break;
+        }
+        return $driver;
+    }
+
+    private function createFilesystemAdapter(array $config): FilesystemCache
+    {
+        if (!isset($config['path'])) {
+            throw new PathNotSet('Path must be set for the filesystem adapter');
+        }
+        return new FilesystemCache($config['path']);
     }
 }
--- tests/Unit/Factories/DoctrineCacheFactoryTest.php
+++ tests/Unit/Factories/DoctrineCacheFactoryTest.php
@@ -13,18 +13,33 @@ final class DoctrineCacheFactoryTest extends SimpleTestCase
     public function testDefault()
     {
         $factory = new DoctrineCacheFactory();
-        $pool = $factory->make([]);
+        $pool = $factory->make([
+            'path' => 'tests/filesystem/cache',
+        ]);
         $this->assertInstanceOf('Cache\Adapter\Doctrine\DoctrineCachePool', $pool);
         $this->assertInstanceOf('Psr\Cache\CacheItemPoolInterface', $pool);
+        $this->assertInstanceOf('Doctrine\Common\Cache\FilesystemCache', $pool->getCache());
     }
 
     public function testFilesystem()
     {
-        $factory = new SymfonyCacheFactory();
+        $factory = new DoctrineCacheFactory();
         $pool = $factory->make([
             'driver' => 'filesystem',
+            'path' => 'tests/filesystem/cache',
         ]);
         $this->assertInstanceOf('Cache\Adapter\Doctrine\DoctrineCachePool', $pool);
         $this->assertInstanceOf('Psr\Cache\CacheItemPoolInterface', $pool);
+        $this->assertInstanceOf('Doctrine\Common\Cache\FilesystemCache', $pool->getCache());
+    }
+
+    public function testFilesystemPathNotSet()
+    {
+        $this->expectException('PublishingKit\Cache\Exceptions\Factories\PathNotSet');
+        $factory = new DoctrineCacheFactory();
+        $config = [
+            'driver' => 'filesystem',
+        ];
+        $pool = $factory->make($config);
     }
 }
